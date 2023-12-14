6e32ef98c5e38a6f942ce20277a2f15d9efe7213
Andrei Kozlov <kyzia.ru@gmail.com>
2023-12-14 14:20:27 +0200

    move swapToVariable to factory

--- src/PoolInstance.sol
+++ src/PoolInstance.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {Pool, DataTypes, BorrowLogic, IPoolAddressesProvider, Errors} from 'aave-v3-factory/core/contracts/protocol/pool/Pool.sol';
+import {Pool, IPoolAddressesProvider, Errors} from 'aave-v3-factory/core/contracts/protocol/pool/Pool.sol';
 
-import {IPoolSwapToVariable} from './IPoolSwapToVariable.sol';
-
-contract PoolInstance is Pool, IPoolSwapToVariable {
+contract PoolInstance is Pool {
   uint256 public constant POOL_REVISION = 3;
 
   constructor(IPoolAddressesProvider provider) Pool(provider) {}
@@ -16,16 +14,5 @@ contract PoolInstance is Pool, IPoolSwapToVariable {
 
   function getRevision() internal pure virtual override returns (uint256) {
     return POOL_REVISION;
-  }
-
-  /// @inheritdoc IPoolSwapToVariable
-  function swapToVariable(address asset, address user) public virtual override {
-    BorrowLogic.executeSwapBorrowRateMode(
-      _reserves[asset],
-      _usersConfig[user],
-      asset,
-      user,
-      DataTypes.InterestRateMode.STABLE
-    );
   }
 }
