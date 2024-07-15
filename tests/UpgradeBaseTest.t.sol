e7d6c31a45ac53d17f3713c2bcd712ce4c45d229
Andrei Kozlov <kyzia.ru@gmail.com>
2024-07-15 20:23:46 +0200

    update blocks

--- tests/UpgradeBaseTest.t.sol
+++ tests/UpgradeBaseTest.t.sol
@@ -7,7 +7,7 @@ import {DeploymentLibrary} from '../scripts/Deploy.s.sol';
 contract UpgradeBaseTest is
   UpgradePayloadTest(
     'base',
-    16212761,
+    17139145,
     1.5 * 1e3 // limit is 0.015%
   )
 {
